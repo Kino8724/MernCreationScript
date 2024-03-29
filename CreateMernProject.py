88c52f4c673e277142ded7130d1a058d1ac79229
Wyatt Ptak <107960001+Kino8724@users.noreply.github.com>
2024-03-28 17:21:23 -0700

    Updating version

--- CreateMernProject.py
+++ CreateMernProject.py
@@ -27,12 +27,44 @@ os.system("mkdir config controllers models routes")
 Path("./config/mongoose.config.js").touch()
 Path("./.env").touch()
 
-# This code will add the necessary boilerplate for the package.json
+# Add the necessary boilerplate for the package.json
 with open(Path("./package.json"), "r") as package:
     data = json.load(package)
-
 data["type"] = "module"
 data["scripts"]["dev"] = "npx nodemon"
-
 with open(Path("./package.json"), "w") as package:
     json.dump(data, package)
+
+# Add the boilerplate inside the mongoose.config.js
+with open(Path("./config/mongoose.config.js"), "w") as mongoose:
+    mongoose.writelines([
+        "import { connect } from 'mongoose';\n",
+        "import dotenv from 'dotenv';\n",
+        "dotenv.config()\n",
+        "const MONGODB_URI = process.env.MONGODB_URI;\n",
+        "async function dbConnect() {\n",
+        "  try {\n",
+        "    await connect(MONGODB_URI, {\n",
+        "    dbName: 'booksDB',\n",
+        "    });\n",
+        "    console.log('Pinged your deployment. You successfully connected to MongoDB!');\n",
+        "  } catch (error) {\n",
+        "    console.log(error);\n",
+        "    throw error;\n",
+        "  }\n",
+        "}\n",
+        "export default dbConnect\n"])
+
+# Add the boilerplate inside the server.js
+with open(Path("./server.js"), "w") as server:
+    server.write("import express from 'express';\n")
+    server.write("import cors from 'cors';\n")
+    server.write("import dotenv from 'dotenv';\n")
+    server.write("import dbConnect from './config/mongoose.config.js';\n")
+    server.write("const app = express();\n")
+    server.write("app.use(express.json(), cors());\n")
+    server.write("dotenv.config();\n")
+    server.write("const PORT = process.env.PORT;\n")
+    server.write("dbConnect();\n")
+    server.write(
+        "app.listen(PORT, console.log(`Listening on port: ${PORT}`)) \n")
